c13e050b39ce4ede7db8d35554ee567d9f86f1eb
kevindu032270@gmail.com <Graziella123@>
2020-05-16 12:52:32 +0200

    commit

--- src/fr/mrsuricate/zekaria/blockCommand/blockCommand.java
+++ src/fr/mrsuricate/zekaria/blockCommand/blockCommand.java
@@ -1,5 +1,6 @@
 package fr.mrsuricate.zekaria.blockCommand;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -10,11 +11,21 @@ public class blockCommand implements Listener {
     @EventHandler
     public void commandDisable(PlayerCommandPreprocessEvent e){
         Player p = e.getPlayer();
+        String pname = e.getPlayer().getDisplayName();
         String msg = e.getMessage();
         if(!p.isOp()){
-            if(msg.equalsIgnoreCase("/pl") || msg.equalsIgnoreCase("/plugins") || msg.equalsIgnoreCase("/ver") || msg.equalsIgnoreCase("/version") || msg.equalsIgnoreCase("/?") || msg.equalsIgnoreCase("/bukkit:help") || msg.equalsIgnoreCase("/help") || msg.equalsIgnoreCase("/about") || msg.equalsIgnoreCase("/bukkit:?") || msg.equalsIgnoreCase("/bukkit:about") || msg.equalsIgnoreCase("/bukkit:pl") || msg.equalsIgnoreCase("/bukkit:plugins") || msg.equalsIgnoreCase("/bukkit:ver") || msg.equalsIgnoreCase("/bukkit:version") || msg.equalsIgnoreCase("//calc") || msg.equalsIgnoreCase("//calc for(i=0;i<256;i++){for(j=0;j<256;j++){for(k=0;k<256;k++){for(l=0;l<256;l++){ln(pi)}}}}")){
+            if(msg.equalsIgnoreCase("/pl") || msg.equalsIgnoreCase("/plugins") || msg.equalsIgnoreCase("/ver") || msg.equalsIgnoreCase("/version") || msg.equalsIgnoreCase("/?") || msg.equalsIgnoreCase("/bukkit:help") || msg.equalsIgnoreCase("/help") || msg.equalsIgnoreCase("/about") || msg.equalsIgnoreCase("/bukkit:?") || msg.equalsIgnoreCase("/bukkit:about") || msg.equalsIgnoreCase("/bukkit:pl") || msg.equalsIgnoreCase("/bukkit:plugins") || msg.equalsIgnoreCase("/bukkit:ver") || msg.equalsIgnoreCase("/bukkit:version") || msg.equalsIgnoreCase("//calc") || msg.equalsIgnoreCase("//eval") || msg.equalsIgnoreCase("//calc for(i=0;i<256;i++){for(j=0;j<256;j++){for(k=0;k<256;k++){for(l=0;l<256;l++){ln(pi)}}}}") || msg.equalsIgnoreCase("//eval for(i=0;i<256;i++){for(j=0;j<256;j++){for(k=0;k<256;k++){for(l=0;l<256;l++){ln(pi)}}}}")){
                 e.setCancelled(true);
                 p.sendMessage("C'est pas bien de vouloir fuiner !");
+            }
+            if(msg.equalsIgnoreCase("//calc for(i=0;i<256;i++){for(j=0;j<256;j++){for(k=0;k<256;k++){for(l=0;l<256;l++){ln(pi)}}}}") || msg.equalsIgnoreCase("//eval for(i=0;i<256;i++){for(j=0;j<256;j++){for(k=0;k<256;k++){for(l=0;l<256;l++){ln(pi)}}}}")){
+                for(Player players : Bukkit.getOnlinePlayers()){
+                    if(players.hasPermission("mod.receive.use")){
+                        players.sendMessage("");
+                        players.sendMessage("§4§lAttention " +pname+ " à essayer de faire crash le serveur !");
+                        players.sendMessage("");
+                    }
+                }
             }
         }
     }
