ed8ce617954a43a46b24973f0002b8f71b6b033b
kevindu032270@gmail.com <Graziella123@>
2020-05-22 23:43:49 +0200

    ajout couleur Rules.java

--- src/fr/mrsuricate/zekaria/CustomEnchant/Enchant.java
+++ src/fr/mrsuricate/zekaria/CustomEnchant/Enchant.java
@@ -76,51 +76,56 @@ public class Enchant extends BukkitRunnable implements Listener, CommandExecutor
                             return;
                         }
                     }
-                    if(meta.getLore().contains("§7Mending I")){
-                        if(inv.getItem(2) != null){
-                            String mending = "§7Mending I";
-                            List<String> list = inv.getItem(0).getItemMeta().getLore();
-                            if(list == null){
-                                ItemStack item = inv.getItem(2);
-                                ItemMeta meta2 = item.getItemMeta();
-                                meta2.setLore(Collections.singletonList(mending));
-                                item.setItemMeta(meta2);
-                                inv.setItem(2,item);
-                                return;
-                            }
-                            if(!list.contains(mending)){
-                                list.add(mending);
-                                ItemStack item = inv.getItem(2);
-                                ItemMeta meta2 = item.getItemMeta();
-                                meta2.setLore(list);
-                                item.setItemMeta(meta2);
-                                inv.setItem(2,item);
-                                return;
+                    if(meta.getLore() != null) {
+                        if(meta.getLore().contains("§7Mending I")){
+                            if(inv.getItem(2) != null){
+                                String mending = "§7Mending I";
+                                List<String> list = inv.getItem(0).getItemMeta().getLore();
+                                if(list == null){
+                                    ItemStack item = inv.getItem(2);
+                                    ItemMeta meta2 = item.getItemMeta();
+                                    meta2.setLore(Collections.singletonList(mending));
+                                    item.setItemMeta(meta2);
+                                    inv.setItem(2,item);
+                                    return;
+                                }
+                                if(!list.contains(mending)){
+                                    list.add(mending);
+                                    ItemStack item = inv.getItem(2);
+                                    ItemMeta meta2 = item.getItemMeta();
+                                    meta2.setLore(list);
+                                    item.setItemMeta(meta2);
+                                    inv.setItem(2,item);
+                                    return;
+                                }
                             }
                         }
                     }
+
                     if(inv.getItem(0) != null){
                         if(inv.getItem(0).getType().equals(Material.LEATHER_HELMET) || inv.getItem(0).getType().equals(Material.IRON_HELMET) || inv.getItem(0).getType().equals(Material.GOLD_HELMET) || inv.getItem(0).getType().equals(Material.CHAINMAIL_HELMET) || inv.getItem(0).getType().equals(Material.DIAMOND_HELMET)){
-                            if(meta.getLore().contains("§7Vision nocturne I")){
-                                if(inv.getItem(2) != null){
-                                    String mending = "§7Vision nocturne I";
-                                    List<String> list = inv.getItem(0).getItemMeta().getLore();
-                                    if(list == null){
-                                        ItemStack item = inv.getItem(2);
-                                        ItemMeta meta2 = item.getItemMeta();
-                                        meta2.setLore(Collections.singletonList(mending));
-                                        item.setItemMeta(meta2);
-                                        inv.setItem(2,item);
-                                        return;
-                                    }
-                                    if(!list.contains(mending)){
-                                        list.add(mending);
-                                        ItemStack item = inv.getItem(2);
-                                        ItemMeta meta2 = item.getItemMeta();
-                                        meta2.setLore(list);
-                                        item.setItemMeta(meta2);
-                                        inv.setItem(2,item);
-                                        return;
+                            if (meta.getLore() != null){
+                                if(meta.getLore().contains("§7Vision nocturne I")){
+                                    if(inv.getItem(2) != null){
+                                        String mending = "§7Vision nocturne I";
+                                        List<String> list = inv.getItem(0).getItemMeta().getLore();
+                                        if(list == null){
+                                            ItemStack item = inv.getItem(2);
+                                            ItemMeta meta2 = item.getItemMeta();
+                                            meta2.setLore(Collections.singletonList(mending));
+                                            item.setItemMeta(meta2);
+                                            inv.setItem(2,item);
+                                            return;
+                                        }
+                                        if(!list.contains(mending)){
+                                            list.add(mending);
+                                            ItemStack item = inv.getItem(2);
+                                            ItemMeta meta2 = item.getItemMeta();
+                                            meta2.setLore(list);
+                                            item.setItemMeta(meta2);
+                                            inv.setItem(2,item);
+                                            return;
+                                        }
                                     }
                                 }
                             }
--- src/fr/mrsuricate/zekaria/commands/Rules.java
+++ src/fr/mrsuricate/zekaria/commands/Rules.java
@@ -14,25 +14,24 @@ public class Rules implements CommandExecutor {
             if (cmd.getName().equalsIgnoreCase("rules")){
 
                 p.sendMessage("");
-
                 p.sendMessage("§4--- §bRéglement§4 ---");
                 p.sendMessage("");
 
-                p.sendMessage("§8[1] §bSur le tchat, restez toujours courtois et limitez vos majuscules.");
-                p.sendMessage("§8[2] §bLes insultes sont sanctionnables.");
-                p.sendMessage("§8[3] §bLe TP-KILL est interdit et sanctionnable avec preuves.");
-                p.sendMessage("§8[4] §bTout skin inapproprié seras sanctionné.");
-                p.sendMessage("§8[5] §bLe spam, le flood, la provocation sont interdits.");
-                p.sendMessage("§8[6] §bLes débats concernant la politique, la religion et le racisme ne seront pas toléré.");
-                p.sendMessage("§8[7] §bToutes usurpation d'identité sera sanctionné.");
-                p.sendMessage("§8[8] §bLa publicité pour d'autres serveur ne sont pas toléré.");
-                p.sendMessage("§8[9] §bSeul les mods optifine, damage indicator, HUD et Schematica sont autorisés.");
-                p.sendMessage("§8[10] §bL'utilisation du market publique à des fins d'extension d'inventaire est interdite.");
-                p.sendMessage("§8[11] §bTout actes commerciale entre joueurs métant en relation une monnaie non virtuelle (dollar, euro) est prohibé sur toute l'infrastructure Zekaria.");
-                p.sendMessage("§8[12] §bL'utilisation de double compte est sanctionnable.");
-                p.sendMessage("§8[13] §bLa trahison est autorisé.");
-                p.sendMessage("§8[14] §bLes use-bug sont interdits.");
-                p.sendMessage("§8[15] §bL'utilisation d'un VPN à des fin malveillante est strictement interdit.");
+                p.sendMessage("§8[1] §bSur le tchat, restez toujours §dcourtois §bet §nlimitez vos majuscules.");
+                p.sendMessage("§8[2] §bLes §cinsultes §bsont sanctionnables.");
+                p.sendMessage("§8[3] §bLe §dTP-KILL §best interdit et sanctionnable §navec preuves.");
+                p.sendMessage("§8[4] §bTout §dskin inapproprié §bseras sanctionné.");
+                p.sendMessage("§8[5] §bLe §dspam§b, le §dflood§b, la §dprovocation §bsont interdits.");
+                p.sendMessage("§8[6] §bLes débats concernant la §dpolitique§b, la §dreligion §bet le §dracisme §bne seront pas toléré.");
+                p.sendMessage("§8[7] §bToutes §dusurpation d'identité §bsera sanctionné.");
+                p.sendMessage("§8[8] §bLa §dpublicité §bpour d'autres serveur ne sera pas toléré.");
+                p.sendMessage("§8[9] §bSeul les mods §doptifine§b, §ddamage indicator§b, §dHUD §bet §dSchematica  §bsont autorisés.");
+                p.sendMessage("§8[10] §bL'utilisation du §6market publique §bà des fins §cd'extension d'inventaire §best interdite.");
+                p.sendMessage("§8[11] §bTout §cactes commerciale entre joueurs §bmétant en relation une §cmonnaie non virtuelle (dollar, euro) §best prohibé sur toute l'infrastructure Zekaria.");
+                p.sendMessage("§8[12] §bL'utilisation de §ddouble compte §best sanctionnable.");
+                p.sendMessage("§8[13] §bLa §dtrahison §best autorisé.");
+                p.sendMessage("§8[14] §bLes §duse-bug §bsont interdits.");
+                p.sendMessage("§8[15] §bL'utilisation d'un §dVPN §bà des fin §cmalveillante §best strictement interdit.");
                 p.sendMessage("§8[16] §bToute atteinte par déni de service conformément à l'article §4§n323-2 §bdu Code Pénal envers toute l'infrastructure de Zekaria.fr sera sanctionné.");
 
                 p.sendMessage("");
